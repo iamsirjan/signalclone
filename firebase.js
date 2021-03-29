6f97266709449e17c5284b63cd01598d936de51c
iamsirjan <sirjan.gnr@gmail.com>
2021-03-30 00:33:11 +0545

    stack screen and head style

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBBXEwdn3WC1qc0kCWy0X062GZrQV5gU-w",
+    authDomain: "signal-clone-fcf64.firebaseapp.com",
+    projectId: "signal-clone-fcf64",
+    storageBucket: "signal-clone-fcf64.appspot.com",
+    messagingSenderId: "925939568088",
+    appId: "1:925939568088:web:cfc531e36dbd3824de36ad",
+    measurementId: "G-R6T0H0P0LY"
+  };
